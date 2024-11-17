659a9d908c78036734e14bf106e144a3184dd7f8
Rodrigo <machadorodrigo@gmail.com>
2024-11-17 17:34:44 +0000

    cmake for visual clean

---
+++ visual/build/CMakeFiles/visual.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/visual.dir/graph.c.o"
+  "CMakeFiles/visual.dir/graph.c.o.d"
+  "CMakeFiles/visual.dir/main.c.o"
+  "CMakeFiles/visual.dir/main.c.o.d"
+  "CMakeFiles/visual.dir/visualization.c.o"
+  "CMakeFiles/visual.dir/visualization.c.o.d"
+  "visual"
+  "visual.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/visual.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
